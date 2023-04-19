7a083c5a17face9e32df2805be427532db679f1d
cj3017184 <cj3017184@LAB01-PC36>
2023-04-19 12:55:53 -0300

    Adicionar arquivos de projeto.

---
+++ Prova1PR2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova1PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+
+            //int num;
+
+            //Console.WriteLine("digite um numero inteiro ");
+            //num = int.Parse(Console.ReadLine());
+
+
+            //if (num < 0)
+            //    num = -num;
+
+            //Console.WriteLine(" O Valor Absoluto é de :" + num);
+
+
+
+
+
+            //Exercicio 2
+
+            //int num;
+
+            //Console.WriteLine(" Digite um número inteiro");
+            //num = int.Parse(Console.ReadLine());
+
+            //if ( num * 3 == 0 && num *
+
+
+
+
+            //Exercicio 3
+
+            //double num1, nume2, num3;
+
+            //Console.WriteLine(" Digite um número inteiro ");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" Digite um número inteiro ");
+            //nume2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" Digite um número inteiro ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //{
+            //    if (num1 > nume2 && num1 > num3) 
+            //    Console.WriteLine("O maior numero é: " + num1);
+
+            //    if (nume2 > num1 && nume2 > num3) 
+            //    Console.WriteLine("o maior numero é: " + nume2);
+
+            //else if (num3 > nume2 && num3 > num1)
+            //    Console.WriteLine("O maior numero é: " + num3);
+
+            //Exercício 4
+            //int numero;
+
+            //Console.WriteLine("======MENU======");
+            //Console.WriteLine("1 - Domingo 1 - ");
+            //Console.WriteLine("2 - Segunda 2  - ");
+            //Console.WriteLine("3 - Terça 3  - ");
+            //Console.WriteLine("4 - Quarta 4 - ");
+            //Console.WriteLine("5 - Quinta 5 - ");
+            //Console.WriteLine("6 - Sexta 6  - ");
+            //Console.WriteLine("7 - Sábado 7  - ");
+
+
+            //numero = int.Parse(Console.ReadLine());
+
+            //int dia;
+            //Console.WriteLine("digite um numero de 1 a 7");
+            //dia = int.Parse(Console.ReadLine());
+            //switch (dia)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Filosofia, Português e Matematíca ");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Química, História e Inglês no mundo da Informatíca ");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Biologia, Português e Programação ");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Fisíca, Redes de Computadores e Sociologia ");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Geografia, Matematica e Banco de Dados ");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("EU jogo bola ");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine(" Durmo ");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("não existe dia da semana representado por este numero");
+            //        break;
+
+
+
+
+            //Exercicio 6 
+           
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
